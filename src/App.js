41d22b944721825a5322a03d9c431dd84cfb639a
Mxrtin-xii <ibrahimzubaircompanies.ae@gmail.com>
2020-04-13 12:08:18 +0100

    Add more files via cli

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Converter from '../src/Components/CConverter';
+
+function App() {
+  return (
+    <div>
+         <Converter />
+            </div>
+  );
+}
+
+export default App;
